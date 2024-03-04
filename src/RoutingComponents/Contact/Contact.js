adfcbe93fdce2a549c2599127db088cd87d22aee
Riyaz1688 <sayyadrz111@gmail.com>
2024-03-04 16:41:28 +0530

    changed'

---
+++ src/RoutingComponents/Contact/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <h1 className='text-center fs-5'>
+      Contact.....Ruchitha
+                  8790463287
+    </h1>
+  )
+}
+
+export default Contact
---
+++ src/RoutingComponents/Home/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className= 'text-center fs-5'>
+      welcome
+    </div>
+  )
+}
+
+export default Home
